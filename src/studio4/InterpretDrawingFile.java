6d91168ff03a98998c6300ae1fdaf4a8bbbcafed
tenzinjamphel <tenzinjamphel@10.232.169.223>
2023-10-04 16:17:40 -0500

    studio4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -12,6 +12,7 @@ import edu.princeton.cs.introcs.StdDraw;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
+
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -19,6 +20,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		double x = in.nextDouble();
+		System.out.println(x);
+		double y = in.nextDouble();
+		System.out.println(y);
+		double halfWidth = in.nextDouble();
+		System.out.println(halfWidth);
+		double halfHeigh = in.nextDouble();
+		System.out.println(halfHeigh);
+	
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeigh);
+		
+		
 		
 	}
 }
